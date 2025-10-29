0ee8a68cf1c56ad5f8503374c424d85bc08cf09a
Rajvee <rajveedkadchha@gmail.com>
2025-10-30 01:50:56 +0530

    sum newline delimiter (RED)

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -7,3 +7,7 @@ test("returns 0 for empty string", () => {
 test("returns sum of comma-separated numbers", () => {
   expect(add("1,2,3")).toBe(6);
 });
+
+test("handles newline as a delimiter along with commas", () => {
+  expect(add("1\n2,3")).toBe(6);
+});
