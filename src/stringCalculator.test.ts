cff00faab73c1f2ad1c559d0054027bc83df2eee
Rajvee <rajveedkadchha@gmail.com>
2025-10-30 01:43:28 +0530

    sum comma-separated(RED)

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -3,3 +3,7 @@ import { add } from "./stringCalculator";
 test("returns 0 for empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("returns sum of comma-separated numbers", () => {
+  expect(add("1,2,3")).toBe(6);
+});
