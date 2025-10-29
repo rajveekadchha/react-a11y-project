646aacb806459570e2342146c464056043129bcc
Rajvee <rajveedkadchha@gmail.com>
2025-10-30 02:11:45 +0530

    negatives throw with list (GREEN)

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -3,6 +3,12 @@ export function add(numbers: string): number {
   
     // Split by comma, convert to numbers, and sum
     const parts = numbers.split(/,|\n/).map(Number);
+
+    const negatives = parts.filter(n => n < 0);
+  if (negatives.length) {
+    throw new Error(`Negative numbers not allowed: ${negatives.join(", ")}`);
+  }
+  
     return parts.reduce((sum, n) => sum + n, 0);
   }
   
